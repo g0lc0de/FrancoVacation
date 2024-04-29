063d82612b4b7a6c551f46226fac79fa45c0254c
Niklas <niklash187@gmail.com>
2024-04-29 02:10:54 +0200

    Initial 2, first test setup and maven figured out!

---
+++ src/main/java/domain/entity/City.java
@@ -0,0 +1,14 @@
+package domain.entity;
+
+public class City {
+
+    int x = 1;
+
+    public City() {
+
+    }
+
+    public int getX() {
+        return x;
+    }
+}
---
+++ src/test/java/domain/entity/CityTest.java
@@ -0,0 +1,15 @@
+package domain.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CityTest {
+
+    @Test
+    void testCity() {
+        City c = new City();
+        assertEquals(c.getX(),1);
+    }
+
+}
